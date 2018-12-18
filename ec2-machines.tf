ba8d32161412bbcaa690e07cda1c308d34e17a5e
Meny <menyb@dev-operation.io>
2018-12-18 15:02:42 +0200

    chnage_grafana_subent

--- ec2-machines.tf
+++ ec2-machines.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "grafana" {
   ami           = "${lookup(var.AmiLinux, var.region)}"
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
-  subnet_id = "${aws_subnet.PrivateAZA.id}"
+  subnet_id = "${aws_subnet.PublicAZA.id}"
   vpc_security_group_ids = ["${aws_security_group.grafana.id}"]
   key_name = "${var.key_name}"
   tags {
