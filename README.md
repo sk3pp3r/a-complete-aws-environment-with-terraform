156b91c0eb1b78e796cbf6e042ae1fe05286337d
Meny <menyb@dev-operation.io>
2018-12-18 10:45:17 +0200

    Change_variable&READMW.md

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # A COMPLETE AWS ENVIRONMENT WITH TERRAFORM
-this repo is only to store the files of the article published on linuxacademy article
 
-https://linuxacademy.com/howtoguides/posts/show/topic/13922-a-complete-aws-environment-with-terraform
-
-You can find full instructions in the article :
+Haim_Choen_DevOps_MidProject
--- variables.tf
+++ variables.tf
@@ -27,23 +27,23 @@ variable "credentialsfile" {
   description = "where your access and secret_key are stored, you create the file when you run the aws config"
 }
 
-variable "vpc-fullcidr" {
+variable "Vpc-Full_cidr" {
     default = "172.28.0.0/16"
-  description = "the vpc cdir"
+  description = "VPC CIDR"
 }
 variable "Subnet-Public-AzA-CIDR" {
   default = "172.28.0.0/24"
-  description = "the cidr of the subnet"
+  description = "Public_Subnet_CIDR"
 }
 variable "Subnet-Private-AzA-CIDR" {
   default = "172.28.3.0/24"
-  description = "the cidr of the subnet"
+  description = "Private_Subnet_CIDR"
 }
 variable "key_name" {
   default = "demotmp"
   description = "the ssh key to use in the EC2 machines"
 }
 variable "DnsZoneName" {
-  default = "linuxacademy.internal"
+  default = "haimcohen.internal"
   description = "the internal dns name"
 }
